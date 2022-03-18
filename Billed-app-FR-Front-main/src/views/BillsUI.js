de420513da34a12bc05e1fc94fcf7941ecce78bc
Quentin Rampteau <quentin425@hotmail.fr>
2022-03-18 15:32:16 +0100

    Fixing login bug

--- Billed-app-FR-Front-main/src/views/BillsUI.js
+++ Billed-app-FR-Front-main/src/views/BillsUI.js
@@ -1,7 +1,6 @@
 import VerticalLayout from './VerticalLayout.js'
 import ErrorPage from "./ErrorPage.js"
 import LoadingPage from "./LoadingPage.js"
-
 import Actions from './Actions.js'
 
 const row = (bill) => {
@@ -20,11 +19,11 @@ const row = (bill) => {
 }
 
 const rows = (data) => {
-  data.sort((a, b) => b.date.localeCompare(a.date));
+  data = data.sort((a, b) => b.date.localeCompare(a.date));
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
-export default ({ data: bills, loading, error }) => {
+export default ({ data: bills = [], loading, error }) => {
   
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
