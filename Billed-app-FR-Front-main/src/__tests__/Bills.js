0e136fc88442cd4eff053ec06a080c1d54fe621f
Quentin Rampteau <quentin425@hotmail.fr>
2022-04-15 14:37:00 +0200

    Alexandre

--- Billed-app-FR-Front-main/src/__tests__/Bills.js
+++ Billed-app-FR-Front-main/src/__tests__/Bills.js
@@ -2,13 +2,16 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {fireEvent, screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
 import router from "../app/Router.js";
+import Bill from "../containers/Bills.js";
+import { ROUTES } from "../constants/routes";
+import mockStore from "../__mocks__/store"
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -25,15 +28,60 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
+      expect(windowIcon.className).toBe("active-icon")
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
+
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+    describe("When I click the eye icon", () => {
+      test("Then the modal should be open", () => {
+
+        $.fn.modal = jest.fn();
+        document.body.innerHTML = BillsUI({ data: bills })
+        const BillTest = new Bill({document, onNavigate, store:null, localStorage:window.localStorage});
+        BillTest.handleClickIconEye(document.querySelectorAll(`div[data-testid="icon-eye"]`)[3]);
+        expect(screen.getAllByText(("Justificatif"))).toBeTruthy()
+      })
+    })
+    describe("When I click the new bill button", () => {
+      test("Then it should navigate to the new bills page", () => {
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        document.body.innerHTML = BillsUI({ data: bills })
+        const BillTest = new Bill({document, onNavigate, store:null, localStorage:window.localStorage});
+        BillTest.handleClickNewBill();
+        expect(screen.getAllByText(("Envoyer une note de frais"))).toBeTruthy()
+      })
+    })
+  })
+})
+
+// test d'intégration GET
+describe("Given I am a user connected as Employee", () => {
+  test("It should fetch bills from the api",async () => {
+    document.body.innerHTML = BillsUI({ data: bills })
+    localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByText("Mes notes de frais"))
+      const billName1  = screen.getByText("encore")
+      expect(billName1).toBeTruthy()
+      const billName2  = screen.getByText("test1")
+      expect(billName2).toBeTruthy()
+      const billName3  = screen.getByText("test2")
+      expect(billName3).toBeTruthy()
+      const billName4  = screen.getByText("test3")
+      expect(billName4).toBeTruthy()
   })
 })
--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,17 +2,64 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
+import ROUTES from "../constants/routes"
+import store from "../app/Store"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the form should appear", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      expect(screen.getByText("Envoyer une note de frais")).toBeTruthy()
+    })
+    describe("When I change the file with a correct format", () => {
+      test("It should change the file", () => {
+        document.body.innerHTML = NewBillUI()
+        const userTest = '{"email":"employee@test.tld"}'
+        localStorage.setItem("user", userTest)
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const NewBillTest = new NewBill({document, onNavigate, store, localStorage:window.localStorage})
+        fireEvent.change(screen.getByTestId("file"), {
+          target: {
+            files: [new File([''], 'test.png', {type: 'image/png'})],
+          }
+        })
+        expect(screen.getByTestId("file").files[0].name).toBe("test.png")
+      })
+    })
+    describe("When I submit the form with correct informations", () => {
+      test("It creates a new Bill with the right informations, and we go to Bill page", () => {
+        document.body.innerHTML = NewBillUI()
+        const userTest = '{"email":"employee@test.tld"}'
+        localStorage.setItem("user", userTest)
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const NewBillTest = new NewBill({document, onNavigate, store, localStorage:window.localStorage})
+        new File([''], 'test.png', {type: 'image/png'})
+        screen.getByTestId("expense-type").value = "Transports"
+        expect(screen.getByTestId("expense-type").value).toBe("Transports")
+        screen.getByTestId("expense-name").value = "Test"
+        expect(screen.getByTestId("expense-name").value).toBe("Test")
+        screen.getByTestId("amount").value = 666
+        expect(screen.getByTestId("amount").value).toBe("666")
+        screen.getByTestId("datepicker").value = "2022-04-06"
+        expect(screen.getByTestId("datepicker").value).toBe("2022-04-06")
+        screen.getByTestId("vat").value = 60
+        expect(screen.getByTestId("vat").value).toBe("60")
+        screen.getByTestId("pct").value = 60
+        expect(screen.getByTestId("pct").value).toBe("60")
+        screen.getByTestId("commentary").value = "Test"
+        expect(screen.getByTestId("commentary").value).toBe("Test")
+        document.getElementById("btn-send-bill").click()
+        console.log("screen",screen)
+        expect(screen.getAllByText("Mes notes de frais")).toBeTruthy()
+      })
     })
   })
 })
